25d72d26961a6274d192e86e58c658daf358bcf0
Rob M <robbis@gmail.com>
2019-06-07 16:37:17 -0700

    Create authenticator.rb

---
+++ authenticator.rb
@@ -0,0 +1,15 @@
+users = [
+          { username: "rob", password: "password1" },
+          { username: "paul", password: "password2" },
+          { username: "dean", password: "password3" },
+          { username: "cat", password: "password4" },
+          { username: "jazz", password: "password5" }
+        ]
+
+  puts "Welcome to the authenticator"
+  25.times { print "-" }
+  puts
+  puts "This program will take an input from the user and compare the respective password"
+  puts "If the password is correct you will get back the user object"
+
+  
